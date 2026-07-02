dda8f529e6dfff3313f79ec90e94696811c475b9
lissy78 <lisa.meneses@correounivalle.edu.co>
2026-07-02 15:46:20 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'tesseract.js': fileURLToPath(
+        new URL('./node_modules/tesseract.js/dist/tesseract.esm.min.js', import.meta.url)
+      ),
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
-    include: ['qrcode', 'tesseract.js'],
+    include: ['qrcode'],
   },
 });
